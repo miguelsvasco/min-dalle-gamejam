0160d3e8344dadcae652c8396bb3fb85485373cd
Miguel <miguelserrasvasco@gmail.com>
2022-09-01 06:52:24 +0100

    Add command line parser to generator (WIP).

--- test_mini_dalle.py
+++ test_mini_dalle.py
@@ -5,13 +5,12 @@ import argparse
 #!/usr/bin/env python
 #\ coding: utf-8
 
-
 # Init model - Mega requires 8GB VRAM minimum
 model = MinDalle(
     models_root='./pretrained/',
-    dtype=torch.float16,
+    dtype=torch.float32,
     device='cuda',
-    is_mega=False,
+    is_mega=True,
     is_reusable=True
 )
 
@@ -23,18 +22,18 @@ parser.add_argument('--seed', type=int, help='random seed to generate')
 
 args = parser.parse_args()
 
-image = model.generate_image(
-    text=args.prompt,
-    seed=args.seed,
-    grid_size=args.n,
-    is_seamless=False,
-    temperature=1,
-    top_k=256,
-    supercondition_factor=32,
-    is_verbose=True
-)
-
-import ipdb; ipdb.set_trace()
-image.save('image.png')
+for i in range(args.n):
+    print(f'Generating image #{i}')
+    image = model.generate_image(
+        text=args.prompt,
+        seed=args.seed,
+        grid_size=1,
+        is_seamless=False,
+        temperature=1,
+        top_k=256,
+        supercondition_factor=32,
+        is_verbose=True
+    )
+    image.save(f'results/image_{i}.png')
 
 print("Finished")
