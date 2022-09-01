f96863b1904c6825394110af9cf803641625e6c2
Miguel <miguelserrasvasco@gmail.com>
2022-09-01 06:39:47 +0100

    Add command line parser to generator (WIP).

--- test_mini_dalle.py
+++ test_mini_dalle.py
@@ -1,9 +1,12 @@
 import torch
 from PIL.Image import Image
 from min_dalle import MinDalle
+import argparse
 #!/usr/bin/env python
 #\ coding: utf-8
 
+
+# Init model - Mega requires 8GB VRAM minimum
 model = MinDalle(
     models_root='./pretrained/',
     dtype=torch.float16,
@@ -12,10 +15,18 @@ model = MinDalle(
     is_reusable=True
 )
 
+# Parse commands
+parser = argparse.ArgumentParser(description='Script to generate from Dall-E mini (mega version).')
+parser.add_argument('--prompt', action='store', type=str,  help='the type of image to create (ANYTHING)')
+parser.add_argument('--n',  type=int,  help='number of images to generate')
+parser.add_argument('--seed', type=int, help='random seed to generate')
+
+args = parser.parse_args()
+
 image = model.generate_image(
-    text='a woman face',
-    seed=-1,
-    grid_size=1,
+    text=args.prompt,
+    seed=args.seed,
+    grid_size=args.n,
     is_seamless=False,
     temperature=1,
     top_k=256,
@@ -23,6 +34,7 @@ image = model.generate_image(
     is_verbose=True
 )
 
+import ipdb; ipdb.set_trace()
 image.save('image.png')
 
 print("Finished")
